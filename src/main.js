47f2c15de95e7f05e3773d4aa58921ebf7e4ba55
Juan Calvopina <juan.calvopina@gmail.com>
2023-06-15 23:27:34 -0500

    Functions

--- src/main.js
+++ src/main.js
@@ -1,8 +1,42 @@
-console.log("hi")
+// clasic function
+function sayHello(firstName = 'JC', age = 0) {
+    const greeting = `Hello ${firstName} ${age}`;
+    return greeting;
+}
 
-let firstName = "Juan"
-let lastName = "Calvopina"
+const result = sayHello('', 12);
+console.log(result);
 
 
-console.log("firstName: " + firstName + ' ' + lastName)
-console.log(`firstName: ${firstName} ${lastName}`)
+// anonymous function
+const sayGoodbye = function (name = 'JC') {
+    return `Goodbye ${name} `
+}
+console.log(sayGoodbye());
+
+
+// narrow function
+const saySomething = (name = 'JC', age = 0) => `Say something ${name} ${age}`;
+console.log(saySomething());
+
+
+// ----------------------------------------------------------------
+// clasic
+function clasicSum(a = 0, b = 0) {
+    return a + b;
+}
+console.log(clasicSum());
+console.log(clasicSum(1, 1));
+
+
+//anonymous function
+const anonymousSum = function (a = 0, b = 0) {
+    return a + b;
+}
+console.log(anonymousSum());
+console.log(anonymousSum(2, 3));
+
+// narrow function
+const narrowSum = (a = 0, b = 0) => a + b;
+console.log(narrowSum())
+console.log(narrowSum(3, 4))
