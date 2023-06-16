e1cabeed89d85d32b1a0ee5b4d5895e29424d950
Juan Calvopina <juan.calvopina@gmail.com>
2023-06-15 23:26:53 -0500

    Variables

---
+++ src/main.js
@@ -0,0 +1,8 @@
+console.log("hi")
+
+let firstName = "Juan"
+let lastName = "Calvopina"
+
+
+console.log("firstName: " + firstName + ' ' + lastName)
+console.log(`firstName: ${firstName} ${lastName}`)
