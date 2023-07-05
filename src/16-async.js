6a39ee55e51498b49bc797786b7d5e3470ba8571
Juan Calvopina <juan.calvopina@gmail.com>
2023-07-05 18:17:59 -0500

    Async

---
+++ src/16-async.js
@@ -0,0 +1,10 @@
+const findAllUsers = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    return await response.json();
+    
+}
+
+const users = await findAllUsers();
+console.log(users);
+
+console.log('hi');
