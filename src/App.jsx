69f4263f6c85c383c4d2f236e384d33d3874aa47
Juan Calvopina <juan.calvopina@gmail.com>
2023-08-13 10:57:26 -0500

    Update App component

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { getBilling } from './services/getBilling'
+import BillingDetailView from './components/BillingDetailView';
+import BillingView from './components/BillingView';
+import ClientView from './components/ClientView';
+import CompanyView from './components/CompanyView';
+
+function App() {
+
+  const {
+    id: userId,
+    name: userName,
+    client: clientData,
+    company: companyData,
+    items: itemsData
+  } = getBilling();
+
+  return (
+    <>
+      <div className="container">
+        <div className="card my-3">
+          <div className="card-header">Billing App</div>
+          <div className="card-body">
+            <BillingView id={userId} name={userName} />
+            <div className="row my-3">
+              <div className="col">
+                <ClientView title={"Datos del cliente"} client={clientData} />
+              </div>
+              <div className="col">
+                <CompanyView title={"Datos de la empresa"} company={companyData} />
+              </div>
+            </div>
+            <BillingDetailView title="Detalles de la factura" items={itemsData} />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
