6541c6c2bf18e04a4f454061d7fddd114a1a0dc0
Juan Calvopina <juan.calvopina@gmail.com>
2023-08-13 10:51:14 -0500

    Add dummy data for billing

---
+++ src/data/dummyBilling.js
@@ -0,0 +1,38 @@
+export const dummyBilling = {
+  id: 10,
+  name: 'Componentes PC',
+  client: {
+    name: 'Pepe',
+    lastName: 'Doe',
+    address: {
+      country: 'United States',
+      city: 'Los Angeles',
+      street: 'One Street',
+      number: 12,
+    },
+  },
+  company: {
+    name: 'New Egg',
+    fiscalNumber: 1234567
+  },
+  items: [
+    {
+      id: 1,
+      product: 'CPU Intel i7',
+      quantity: 1,
+      price: 499,
+    },
+    {
+      id: 2,
+      product: 'Corsair Keyboard mecánico',
+      quantity: 1,
+      price: 150,
+    },
+    {
+      id: 3,
+      product: 'Monito Asus',
+      quantity: 1,
+      price: 150,
+    },
+  ]
+}
