b5920c75faaf4c6f7012b8f802a56de4e1814768
Juan Calvopina <juan.calvopina@gmail.com>
2023-08-13 11:30:00 -0500

    Calculate unit total and total for items

--- src/services/getBilling.js
+++ src/services/getBilling.js
@@ -1,7 +1,12 @@
 import { dummyBilling } from "../data/dummyBilling"
 
 export const getBilling = () => {
-  return dummyBilling;
+  let total = 0;
+  dummyBilling.items.forEach(item => {
+    item.unitTotal = item.price * item.quantity,
+    total = total + item.price * item.quantity;
+  })
+  return {...dummyBilling, total};
 }
 
 export default getBilling
