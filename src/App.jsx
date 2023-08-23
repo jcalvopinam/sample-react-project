0b14e6ec2875c01f706b37dedc7eb88a48d03729
Juan Calvopina <juan.calvopina@gmail.com>
2023-08-22 21:38:01 -0500

    HTML products mockup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <h3>Shopping Cart</h3>
+        <div className="row">
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Teclado Mecánido RGB</h5>
+                <p className="card-text">Descripción</p>
+                <p className="card-text">$ 1000</p>
+                <button className="btn btn-primary">Agregar</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Samsung Smart TV 55</h5>
+                <p className="card-text">Descripción</p>
+                <p className="card-text">$ 3000</p>
+                <button className="btn btn-primary">Agregar</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Audífono Bluetooth Sony</h5>
+                <p className="card-text">Descripción</p>
+                <p className="card-text">$ 770</p>
+                <button className="btn btn-primary">Agregar</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Memoria Corsair 8GB DDR5</h5>
+                <p className="card-text">Descripción</p>
+                <p className="card-text">$ 3000</p>
+                <button className="btn btn-primary">Agregar</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Asus Nvidia RTX</h5>
+                <p className="card-text">Descripción</p>
+                <p className="card-text">$ 5000</p>
+                <button className="btn btn-primary">Agregar</button>
+              </div>
+            </div>
+          </div>
+          <div className="col-4 my-2">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">CPU Intel Core i5</h5>
+                <p className="card-text">Descripción</p>
+                <p className="card-text">$ 4000</p>
+                <button className="btn btn-primary">Agregar</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
